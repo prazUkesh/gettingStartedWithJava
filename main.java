305556eb6d870fcbdcb160f2dac44868fa8244bc
prazUkesh <esh3ac@gmail.com>
2025-01-11 14:01:52 +0545

    adding files to github

--- main.java
+++ main.java
@@ -1,7 +1,11 @@
+import javax.print.attribute.standard.PrinterLocation;
+
 public class main {
 
     public static void main(String[] args){
-        System.out.println("kneegamaniiii haye");
+        System.out.println("hi this is Ukesh, FDay 1 of learning Java");
+        System.out.println("chdu gaye guruuuuuuu");
         System.out.println("suspribe");
+        /// wat du hellllllll
     }
 }
