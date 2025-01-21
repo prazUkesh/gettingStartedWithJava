d75cef462b06184a9b41101c193bb6de0d0d3ff0
prazUkesh <esh3ac@gmail.com>
2025-01-21 23:18:47 +0545

    type casting

--- main.java
+++ main.java
@@ -1,7 +1,9 @@
 public class main {
     public static void main(String[] args){
         // real life example of data types
-        int items = 4;
+      /*   int items = 4;
+
+
         float itemPrice = 5.57f;
         float totalCose = items * itemPrice;
         char currency = '$';
@@ -11,6 +13,22 @@ public class main {
         System.out.println("your total cost: "+ currency + totalCose);
 
 
-        System.out.println("---------------------\nthanks you for buying\ndo visit us again");
-    }
+        System.out.println("---------------------\nthanks you for buying\ndo visit us again");*/
+        // widening casting
+       /*  int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+*/
+
+        // narrow casting
+          /*
+           double myDouble = 99.9d;
+          int myInt = (int) myDouble;
+    
+          System.out.println(myInt);
+          System.out.println(myDouble);
+           */
+        }
 }
