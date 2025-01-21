85f8719acde7c3779f9e1e40593bc47566e84520
prazUkesh <esh3ac@gmail.com>
2025-01-21 22:56:53 +0545

    data types in Java, with example code

--- main.java
+++ main.java
@@ -1,11 +1,16 @@
-import javax.print.attribute.standard.PrinterLocation;
-
 public class main {
-
     public static void main(String[] args){
-        System.out.println("hi this is Ukesh, FDay 1 of learning Java");
-        System.out.println("chdu gaye guruuuuuuu");
-        System.out.println("suspribe");
-        /// wat du hellllllll
+        // real life example of data types
+        int items = 4;
+        float itemPrice = 5.57f;
+        float totalCose = items * itemPrice;
+        char currency = '$';
+
+        System.out.println("total items: "+ items);
+        System.out.println("item price per price: "+ itemPrice);
+        System.out.println("your total cost: "+ currency + totalCose);
+
+
+        System.out.println("---------------------\nthanks you for buying\ndo visit us again");
     }
 }
